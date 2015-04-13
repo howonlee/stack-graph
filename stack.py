f6d438bb15e8429e686cbf8629446d23fff12e04
Howon Lee <howon@howonlee.com>
2015-04-13 16:14:16 -0700

    add words

--- stack.py
+++ stack.py
@@ -3,6 +3,24 @@ import scipy.signal as sci_sig
 import matplotlib.pyplot as plt
 import collections
 
+def word_load(filename="corpus.txt"):
+    with open(filename, "r") as corpus_file:
+        words = corpus_file.read().split()
+    curr_idx = 0
+    word_map = {}
+    word_idx = []
+    for word in words:
+        if word in word_map:
+            word_idx.append(word_map[word])
+        else:
+            curr_idx += 1
+            word_map[word] = curr_idx
+            word_idx.append(curr_idx)
+    return word_idx, word_map
+
+def flip(dct):
+    return {v:k for k,v in dct.iteritems()}
+
 def logistic_map(x_n):
     r = 3.8 #chaos
     return r * x_n * (1 - x_n)
@@ -29,7 +47,7 @@ def traverse_stack_graph(graph, start):
         path.append(curr_member)
     return path
 
-if __name__ == "__main__":
+def logit_map():
     xs = []
     curr_x = 0.5
     for x in xrange(1000):
@@ -37,17 +55,27 @@ if __name__ == "__main__":
         curr_x = x_i
         xs.append(x_i)
     xs = np.array(xs)
-    xs, idx = discretize(xs)
-    #idx = idx[:10]
-    list_idx = list(idx)
-    graph = stack_graph(idx)
-    path = traverse_stack_graph(graph, idx[1])
-    xs_new = xs[path]
+    return discretize(xs)
 
-    plt.plot(xs_new)
-    plt.plot(xs)
+def path_to_words(path, flipped_map):
+    words = []
+    for word in path:
+        words.append(flipped_map[word])
+    return words
+
+
+if __name__ == "__main__":
+    #idx = idx[:10]
+    word_idx, word_map = word_load()
+    word_graph = stack_graph(word_idx)
+    flipped_map = flip(word_map)
+    word_path = traverse_stack_graph(word_graph, word_idx[1])
+    print path_to_words(word_path[1000:1100], flipped_map)
+
+    #plt.plot(np.array(word_idx[:500]))
+    #plt.plot(np.array(word_path[:500]))
     #f, pxx_den = sci_sig.periodogram(xs_new)
     #f2, pxx_den2 = sci_sig.periodogram(xs)
     #plt.semilogy(f, pxx_den)
     #plt.semilogy(f2, pxx_den2)
-    plt.show()
+    #plt.show()
